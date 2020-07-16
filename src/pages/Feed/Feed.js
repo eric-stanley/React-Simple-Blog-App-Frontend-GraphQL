e3754abad72f9cc9ef3f5ef89ecca91dc793dbb8
Eric Stanley <ericstanley@pop-os.localdomain>
2020-07-16 21:05:55 +0530

    Changed add post method back to PUT

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -183,7 +183,7 @@ class Feed extends Component {
       formData.append('oldPublicId', this.state.editPost.imagePublicId);
     }
     fetch(process.env.REACT_APP_API_URL + '/post-image', {
-      method: 'POST',
+      method: 'PUT',
       headers: {
         Authorization: 'Bearer ' + this.props.token
       },
